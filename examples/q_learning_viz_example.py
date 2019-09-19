875932f4ad17457a5bcf29ae384f530138501e8c
Neev Parikh <neev_parikh@brown.edu>
2019-09-18 23:24:05 -0400

    Formatting

--- examples/q_learning_viz_example.py
+++ examples/q_learning_viz_example.py
@@ -7,23 +7,39 @@ import argparse
 # Other imports.
 import srl_example_setup
 from simple_rl.agents import QLearningAgent, RMaxAgent
-from simple_rl.run_experiments import run_single_agent_on_mdp 
+from simple_rl.run_experiments import run_single_agent_on_mdp
 from simple_rl.tasks import FourRoomMDP, TaxiOOMDP, GridWorldMDP
 from simple_rl.tasks.grid_world.GridWorldMDPClass import make_grid_world_from_file
 from simple_rl.planning import ValueIteration
 
+
 def parse_args():
     # Add all arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", type=str, default="learning", nargs='?', help="Choose the visualization type (one of {value, policy, agent, learning or interactive}).")
+    parser.add_argument(
+        "-v",
+        type=str,
+        default="learning",
+        nargs='?',
+        help=
+        "Choose the visualization type (one of {value, policy, agent, learning or interactive})."
+    )
     args = parser.parse_args()
     return args.v
 
+
 def main():
-    
+
     # Setup MDP, Agents.
-    mdp = GridWorldMDP(width=7, height=7, init_loc=(1, 1), goal_locs=[(7, 7)], lava_locs=[(4, 2)], gamma=0.95, walls=[(2, 2)], slip_prob=0.1)
-    ql_agent = QLearningAgent(mdp.get_actions(), epsilon=0.95, alpha=0.2) 
+    mdp = GridWorldMDP(width=7,
+                       height=7,
+                       init_loc=(1, 1),
+                       goal_locs=[(7, 7)],
+                       lava_locs=[(4, 2)],
+                       gamma=0.95,
+                       walls=[(2, 2)],
+                       slip_prob=0.1)
+    ql_agent = QLearningAgent(mdp.get_actions(), epsilon=0.95, alpha=0.2)
     viz = parse_args()
 
     # Choose viz type.
@@ -48,10 +64,14 @@ def main():
     elif viz == "learning":
         # --> Press <r> to reset.
         # Show agent's interaction with the environment.
-        mdp.visualize_learning(ql_agent, delay=0.005, num_ep=500, num_steps=200)
+        mdp.visualize_learning(ql_agent,
+                               delay=0.005,
+                               num_ep=500,
+                               num_steps=200)
     elif viz == "interactive":
         # Press <1>, <2>, <3>, and so on to execute action 1, action 2, etc.
-    	mdp.visualize_interaction()
+        mdp.visualize_interaction()
+
 
 if __name__ == "__main__":
     main()
